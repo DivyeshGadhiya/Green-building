2d8aaf6fa277b8aa5644788a4426c9437fdeb8e8
Harshil-sitapara <harshilsitapara4@gmail.com>
2024-07-27 22:08:57 +0530

    Header for service/expertise

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,19 +2,24 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ThemeToggler from "./ThemeToggler";
 import menuData from "./menuData";
+import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowDown } from "react-icons/io";
+
 
 const Header = () => {
-  // Navbar toggle
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+  const [childIndex, setChildIndex] = useState<number | null>(null);
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const [sticky, setSticky] = useState(false);
   const [navbarOpen, setNavbarOpen] = useState(false);
   const navbarToggleHandler = () => {
     setNavbarOpen(!navbarOpen);
   };
 
   // Sticky Navbar
-  const [sticky, setSticky] = useState(false);
   const handleStickyNavbar = () => {
     if (window.scrollY >= 80) {
       setSticky(true);
@@ -26,153 +31,165 @@ const Header = () => {
     window.addEventListener("scroll", handleStickyNavbar);
   });
 
-  // submenu handler
-  const [openIndex, setOpenIndex] = useState(-1);
-  const handleSubmenu = (index) => {
-    if (openIndex === index) {
-      setOpenIndex(-1);
-    } else {
-      setOpenIndex(index);
+
+  const clearTimeoutRef = () => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+      timeoutRef.current = null;
     }
   };
 
-  const usePathName = usePathname();
+  const handleMouseEnter = (index: number) => {
+    clearTimeoutRef();
+    setActiveIndex(index);
+  };
 
+  const handleMouseLeave = () => {
+    timeoutRef.current = setTimeout(() => {
+      setActiveIndex(null);
+      setChildIndex(null);
+    }, 200); // Adjust delay as needed
+  };
+
+  const handleChildMouseEnter = (subIndex: number) => {
+    clearTimeoutRef();
+    setChildIndex(subIndex);
+  };
+
+  const handleChildMouseLeave = () => {
+    timeoutRef.current = setTimeout(() => {
+      setChildIndex(null);
+    }, 200); // Adjust delay as needed
+  };
+
+  const usePathName = usePathname();
   return (
-    <>
-      <header
-        className={`header left-0 top-0 z-40 flex w-full items-center ${
-          sticky
-            ? "fixed z-[9999] bg-white !bg-opacity-80 shadow-sticky backdrop-blur-sm transition"
-            : "absolute bg-transparent  backdrop-blur-md"
+    <header
+      className={`header left-0 top-0 z-40 flex w-full items-center ${sticky
+        ? "fixed z-[9999] bg-white !bg-opacity-80 shadow-sticky backdrop-blur-sm transition"
+        : "absolute bg-transparent  backdrop-blur-md"
         }`}
-      >
-        <div className="container">
-          <div className="relative -mx-4 flex items-center justify-between">
-            <div className="w-60 max-w-full px-4 xl:mr-12">
-              <Link
-                href="/"
-                className={`header-logo block w-full ${
-                  sticky ? "py-5 lg:py-2" : "py-2"
+    >
+      <div className="container">
+        <div className="relative -mx-4 flex items-center justify-between">
+          <div className="w-60 max-w-full px-4 xl:mr-12">
+            <Link
+              href="/"
+              className={`header-logo block w-full ${sticky ? "py-5 lg:py-2" : "py-2"
                 } `}
+            >
+              <Image
+                src="/images/logo/green_logo.svg"
+                alt="logo"
+                width={140}
+                height={30}
+                className="w-full"
+              />
+            </Link>
+          </div>
+          <div className="flex w-full items-center justify-between px-4">
+            <div>
+              <button
+                onClick={navbarToggleHandler}
+                id="navbarToggler"
+                aria-label="Mobile Menu"
+                className="absolute right-4 top-1/2 block translate-y-[-50%] rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden"
               >
-                <Image
-                  src="/images/logo/green_logo.svg"
-                  alt="logo"
-                  width={140}
-                  height={30}
-                  className="w-full"
+                <span
+                  className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 ${navbarOpen ? " top-[7px] rotate-45" : " "
+                    }`}
                 />
-              </Link>
-            </div>
-            <div className="flex w-full items-center justify-between px-4">
-              <div>
-                <button
-                  onClick={navbarToggleHandler}
-                  id="navbarToggler"
-                  aria-label="Mobile Menu"
-                  className="absolute right-4 top-1/2 block translate-y-[-50%] rounded-lg px-3 py-[6px] ring-primary focus:ring-2 lg:hidden"
-                >
-                  <span
-                    className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 ${
-                      navbarOpen ? " top-[7px] rotate-45" : " "
+                <span
+                  className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 ${navbarOpen ? "opacity-0 " : " "
                     }`}
-                  />
-                  <span
-                    className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 ${
-                      navbarOpen ? "opacity-0 " : " "
+                />
+                <span
+                  className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 ${navbarOpen ? " top-[-8px] -rotate-45" : " "
                     }`}
-                  />
-                  <span
-                    className={`relative my-1.5 block h-0.5 w-[30px] bg-black transition-all duration-300 ${
-                      navbarOpen ? " top-[-8px] -rotate-45" : " "
-                    }`}
-                  />
-                </button>
-                <nav
-                  id="navbarCollapse"
-                  className={`navbar absolute right-0 z-30 w-[250px] rounded border-[.5px] border-body-color/50 bg-white px-6 py-4 duration-300 lg:visible lg:static lg:w-auto lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 ${
-                    navbarOpen
-                      ? "visibility top-full opacity-100"
-                      : "invisible top-[120%] opacity-0"
+                />
+              </button>
+              <nav
+                id="navbarCollapse"
+                className={`navbar absolute right-0 z-30 w-[250px] rounded border-[.5px] border-body-color/50 bg-white px-6 py-4 duration-300 lg:visible lg:static lg:w-auto lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 ${navbarOpen
+                  ? "visibility top-full opacity-100"
+                  : "invisible top-[120%] opacity-0"
                   }`}
-                >
-                  <ul className="block lg:flex lg:space-x-12">
-                    {menuData.map((menuItem, index) => (
-                      <li key={index} className="group relative">
-                        {menuItem.path ? (
-                          <Link
-                            href={menuItem.path}
-                            className={`flex py-2 text-base lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 ${
-                              usePathName === menuItem.path
-                                ? "text-primary"
-                                : "text-dark hover:text-primary"
+              >
+                <ul className="block lg:flex lg:space-x-12">
+                  {menuData.map((menu, index) => (
+                    <div
+                      key={menu.id}
+                      className="group relative"
+                      onMouseEnter={() => handleMouseEnter(index)}
+                      onMouseLeave={handleMouseLeave}
+                    >
+                      <Link href={menu.path || ''} className={`flex text-base lg:mr-0 lg:inline-flex lg:px-0 lg:py-6 ${usePathName === menu.path
+                        ? "text-primary"
+                        : "text-dark hover:text-primary"
+                        }`}>
+                        <div className="flex cursor-pointer items-center justify-between text-base text-dark group-hover:text-primary lg:mr-0 lg:inline-flex lg:px-0">{menu.title}
+                          {menu.submenu &&
+                            <IoIosArrowDown className="pl-3 w-[30px] h-25" />
+                          }
+                        </div>
+                      </Link>
+                      {menu.submenu && (
+                        <p
+                          className={`relative left-0 top-full rounded-sm bg-white transition-[top] duration-300 group-hover:opacity-100 lg:invisible lg:absolute lg:top-[110%] lg:block w-[290px] lg:p-4 lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full ${activeIndex !== index ? "block" : "hidden"
                             }`}
-                          >
-                            {menuItem.title}
-                          </Link>
-                        ) : (
-                          <>
-                            <p
-                              onClick={() => handleSubmenu(index)}
-                              className="flex cursor-pointer items-center justify-between py-2 text-base text-dark group-hover:text-primary  lg:mr-0 lg:inline-flex lg:px-0 lg:py-6"
+                        >
+                          {menu.submenu.map((submenu, subIndex) => (
+                            <li
+                              key={submenu.id}
+                              className="relative"
+                              onMouseEnter={() => handleChildMouseEnter(subIndex)}
+                              onMouseLeave={handleChildMouseLeave}
                             >
-                              {menuItem.title}
-                              <span className="pl-3">
-                                <svg width="25" height="24" viewBox="0 0 25 24">
-                                  <path
-                                    fillRule="evenodd"
-                                    clipRule="evenodd"
-                                    d="M6.29289 8.8427C6.68342 8.45217 7.31658 8.45217 7.70711 8.8427L12 13.1356L16.2929 8.8427C16.6834 8.45217 17.3166 8.45217 17.7071 8.8427C18.0976 9.23322 18.0976 9.86639 17.7071 10.2569L12 15.964L6.29289 10.2569C5.90237 9.86639 5.90237 9.23322 6.29289 8.8427Z"
-                                    fill="currentColor"
-                                  />
-                                </svg>
-                              </span>
-                            </p>
-                            <div
-                              className={`submenu relative left-0 top-full rounded-sm bg-white transition-[top] duration-300 group-hover:opacity-100 lg:invisible lg:absolute lg:top-[110%] lg:block lg:w-[250px] lg:p-4 lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full ${
-                                openIndex === index ? "block" : "hidden"
-                              }`}
-                            >
-                              {menuItem?.submenu.map((submenuItem, index) => (
-                                <Link
-                                  href={submenuItem.path}
-                                  key={index}
-                                  className="block rounded py-2.5 text-sm text-dark hover:text-primary lg:px-3"
-                                >
-                                  {submenuItem.title}
-                                </Link>
-                              ))}
-                            </div>
-                          </>
-                        )}
-                      </li>
-                    ))}
-                  </ul>
-                </nav>
-              </div>
-              <div className="flex items-center justify-end pr-16 lg:pr-0">
-                {/* <Link
+                              <Link href={submenu.path || ''}>
+                                <div className="flex justify-between rounded py-2.5 text-sm text-dark hover:text-primary lg:px-3 ">
+                                  {submenu.title}
+                                  <IoIosArrowForward className="inline-block text-end" />
+                                </div>
+                              </Link>
+                              {submenu.childSubMenu && (
+                                <ul className={`absolute left-full w-[210px] top-0 ml-3 bg-white p-4 rounded-md shadow-lg ${childIndex === subIndex ? 'block' : 'hidden'}`}>
+                                  {submenu.childSubMenu.map((child, index) => (
+                                    <Link href={child.path || ''} key={index} className="block rounded py-2.5 text-sm text-dark hover:text-primary lg:px-3">
+                                      {child.title}
+                                    </Link>
+                                  ))}
+                                </ul>
+                              )}
+                            </li>
+                          ))}
+                        </p>
+                      )}
+                    </div>
+                  ))}
+                </ul>
+              </nav>
+            </div>
+            <div className="flex items-center justify-end pr-16 lg:pr-0">
+              {/* <Link
                   href="/signin"
                   className="hidden px-7 py-3 text-base font-medium text-dark hover:opacity-70 md:block"
                 >
                   Sign In
                 </Link> */}
-                <Link
-                  href="#"
-                  className="ease-in-up shadow-btn hover:shadow-btn-hover hidden rounded-sm bg-[#2c9e4b] px-8 py-3 text-base font-medium text-white transition duration-300 hover:bg-opacity-90 md:block md:px-9 lg:px-6"
-                >
-                  Enquiry Now
-                </Link>
-                {/* <div>
+              <Link
+                href="#"
+                className="ease-in-up shadow-btn hover:shadow-btn-hover hidden rounded-sm bg-[#2c9e4b] px-8 py-3 text-base font-medium text-white transition duration-300 hover:bg-opacity-90 md:block md:px-9 lg:px-6"
+              >
+                Enquiry Now
+              </Link>
+              {/* <div>
                   <ThemeToggler />
                 </div> */}
-              </div>
             </div>
           </div>
         </div>
-      </header>
-    </>
+      </div>
+    </header>
   );
 };
 
--- src/components/Header/menuData.tsx
+++ src/components/Header/menuData.tsx
@@ -2,83 +2,266 @@ import { Menu } from "@/types/menu";
 
 const menuData: Menu[] = [
   {
-    id: 1,
-    title: "Home",
-    path: "/",
-    newTab: false,
-  },
-  {
     id: 2,
     title: "About",
-    path: "/about",
-    newTab: false,
-  },
-  {
-    id: 33,
-    title: "Blog",
     path: "#",
     newTab: false,
   },
   {
     id: 3,
+    title: "Expertise",
+    newTab: false,
+    submenu: [
+      {
+        id: 41,
+        title: "Auditing",
+        // path: "/auditing",
+        newTab: false,
+        childSubMenu: [
+          {
+            id: 441,
+            title: "Energy",
+            path: "/energy",
+            newTab: false,
+          },
+          {
+            id: 442,
+            title: "Fire",
+            path: "/fire",
+            newTab: false,
+          },
+          {
+            id: 443,
+            title: "Waste Management",
+            path: "/waste-management",
+            newTab: false,
+          },
+          {
+            id: 444,
+            title: "Water",
+            path: "/water",
+            newTab: false,
+          }
+        ]
+      },
+      {
+        id: 42,
+        title: "Building Engineering",
+        // path: "/building-engineering",
+        newTab: false,
+        childSubMenu: [
+          {
+            id: 421,
+            title: "BIM",
+            path: "/bim",
+            newTab: false,
+          },
+          {
+            id: 422,
+            title: "MEPF Design",
+            path: "/mepf-design",
+            newTab: false,
+          }
+        ]
+      },
+      {
+        id: 43,
+        title: "Building Performance",
+        // path: "/building-performance",
+        newTab: false,
+        childSubMenu: [
+          {
+            id: 431,
+            title: "Energy Modelling",
+            path: "/energy-modelling",
+            newTab: false,
+          },
+          {
+            id: 432,
+            title: "Simulation",
+            path: "/simulation",
+            newTab: false,
+          }
+        ]
+      },
+      {
+        id: 43,
+        title: "Compliances",
+        // path: "/compliances",
+        newTab: false,
+        childSubMenu: [
+          {
+            id: 431,
+            title: "Category View",
+            path: "/blog/category",
+            newTab: false,
+          },
+          {
+            id: 432,
+            title: "Tag View",
+            path: "/blog/tag",
+            newTab: false,
+          }
+        ]
+      },
+      {
+        id: 43,
+        title: "Green Building Facilitation",
+        // path: "/breen-building-facilitation",
+        newTab: false,
+        childSubMenu: [
+          {
+            id: 431,
+            title: "Category View",
+            path: "/blog/category",
+            newTab: false,
+          },
+          {
+            id: 432,
+            title: "Tag View",
+            path: "/blog/tag",
+            newTab: false,
+          }
+        ]
+      },
+      {
+        id: 43,
+        title: "Policy Framework Development",
+        // path: "/policy-framework-development",
+        newTab: false,
+        childSubMenu: [
+          {
+            id: 431,
+            title: "Sustainability Reporting",
+        path: "/sustainability-reporting",
+            newTab: false,
+          },
+          {
+            id: 432,
+            title: "Sustainable Development Policies",
+            path: "/sustainable-development-policies",
+            newTab: false,
+          }
+        ]
+      },
+      {
+        id: 43,
+        title: "Training & Capacity Building",
+        // path: "/training-capacity-building",
+        newTab: false,
+        childSubMenu: [
+          {
+            id: 431,
+            title: "Category View",
+            path: "/blog/category",
+            newTab: false,
+          },
+          {
+            id: 432,
+            title: "Tag View",
+            path: "/blog/tag",
+            newTab: false,
+          }
+        ]
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Blog",
+    path: "#",
+    newTab: false,
+    // submenu: [
+    //   {
+    //     id: 41,
+    //     title: "Latest Posts",
+    //     path: "/blog/latest",
+    //     newTab: false,
+    //     childSubMenu: [
+    //       {
+    //         id: 411,
+    //         title: "Featured Posts",
+    //         path: "/blog/latest/featured",
+    //         newTab: false,
+    //       },
+    //       {
+    //         id: 412,
+    //         title: "Most Read",
+    //         path: "/blog/latest/most-read",
+    //         newTab: false,
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     id: 42,
+    //     title: "Categories",
+    //     path: "/blog/categories",
+    //     newTab: false,
+    //     childSubMenu: [
+    //       {
+    //         id: 421,
+    //         title: "Technology",
+    //         path: "/blog/categories/technology",
+    //         newTab: false,
+    //       },
+    //       {
+    //         id: 422,
+    //         title: "Business",
+    //         path: "/blog/categories/business",
+    //         newTab: false,
+    //       }
+    //     ]
+    //   }
+    // ]
+  },
+  {
+    id: 5,
     title: "Support",
     path: "#",
     newTab: false,
+    // submenu: [
+    //   {
+    //     id: 51,
+    //     title: "FAQ",
+    //     path: "/support/faq",
+    //     newTab: false,
+    //     childSubMenu: [
+    //       {
+    //         id: 511,
+    //         title: "General Questions",
+    //         path: "/support/faq/general",
+    //         newTab: false,
+    //       },
+    //       {
+    //         id: 512,
+    //         title: "Technical Issues",
+    //         path: "/support/faq/technical",
+    //         newTab: false,
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     id: 52,
+    //     title: "Help Center",
+    //     path: "/support/help-center",
+    //     newTab: false,
+    //     childSubMenu: [
+    //       {
+    //         id: 521,
+    //         title: "Tutorials",
+    //         path: "/support/help-center/tutorials",
+    //         newTab: false,
+    //       },
+    //       {
+    //         id: 522,
+    //         title: "Live Chat",
+    //         path: "/support/help-center/live-chat",
+    //         newTab: false,
+    //       }
+    //     ]
+    //   }
+    // ]
   },
-  // {
-  //   id: 4,
-  //   title: "Pages",
-  //   newTab: false,
-  //   submenu: [
-  //     {
-  //       id: 42,
-  //       title: "Contact Page",
-  //       // path: "/contact",
-  //       path: "/",
-  //       newTab: false,
-  //     },
-  //     {
-  //       id: 43,
-  //       title: "Blog Grid Page",
-  //       // path: "/blog",
-  //       path: "/",
-  //       newTab: false,
-  //     },
-  //     {
-  //       id: 44,
-  //       title: "Blog Sidebar Page",
-  //       // path: "/blog-sidebar",
-  //       path: "/",
-  //       newTab: false,
-  //     },
-  //     {
-  //       id: 45,
-  //       title: "Blog Details Page",
-  //       // path: "/blog-details",
-  //       path: "/",
-  //       newTab: false,
-  //     },
-  //     {
-  //       id: 46,
-  //       title: "Sign In Page",
-  //       // path: "/signin",
-  //       path: "/",
-  //       newTab: false,
-  //     },
-  //     {
-  //       id: 47,
-  //       title: "Sign Up Page",
-  //       // path: "/signup",
-  //       path: "/",
-  //       newTab: false,
-  //     },
-  //     {
-  //       id: 48,
-  //       title: "Error Page",
-  //       path: "/error",
-  //       newTab: false,
-  //     },
-  //   ],
-  // },
 ];
+
 export default menuData;
